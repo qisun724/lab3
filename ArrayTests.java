0c0cef0517ed602a3b4a846f159b6096d7b8519d
qisun724 <81499808+qisun724@users.noreply.github.com>
2023-04-20 12:58:34 -0700

    First and Second Method in ArrayExamples fixed

--- ArrayTests.java
+++ ArrayTests.java
@@ -5,14 +5,20 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    int[] input2 = {1, 2, 3};
     ArrayExamples.reverseInPlace(input1);
+    ArrayExamples.reverseInPlace(input2);
     assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{3, 2, 1}, input2);
 	}
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int[] input2 = { 4, 5, 6};
+    int[] input3 = {10, 20, 30, 50};
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{50, 30, 20, 10}, ArrayExamples.reversed(input3));
   }
 }
